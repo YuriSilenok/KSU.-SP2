8053e8fe263d7c1ac27e34a9adddb1f1183444e9
Юрий Силенок <yuri.silenok@gmail.com>
2025-11-21 07:39:26 +0000

    aa

---
+++ contest/1999/a_poli.py
@@ -0,0 +1,9 @@
+# for _ in range(int(input())):
+#     a, b = map(int, input())
+#     print(a + b)
+
+for _ in range(int(input())):
+    n = int(input())
+    a = n // 10
+    b = n % 10
+    print(a + b)
